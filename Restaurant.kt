70000410c177a02b5a9dc30091a5e64e3774c45f
Adithya K <a.dty.3403@gmail.com>
2022-04-16 12:24:11 +0530

    var to val

--- Restaurant.kt
+++ Restaurant.kt
@@ -1,21 +1,21 @@
 fun main() {
     val item = arrayOf("Idly", "Vada")
-    var price = arrayOf(25, 30)
-    var choice = arrayOf(0, 0)
+    val price = arrayOf(25, 30)
+    val choice = arrayOf(0, 0)
     println("Item name\tPrice")
     for (i in 0 until item.size) {
         println(item[i] + "\t\t" + price[i])
     }
     while (true) {
         print("Select an item or press 's' to exit : ")
-        var iname: String = readLine()!!
+        val iname: String = readLine()!!
         if (iname == "s") {
             break
         }
         if (iname in item) {
-            var id = item.indexOf(iname)
+            val id = item.indexOf(iname)
             print("Enter quantity : ")
-            var qn = readLine()!!.toInt()
+            val qn = readLine()!!.toInt()
             choice[id] = qn
         } else println("Enter valid item")
     }
